147b1ca28d97943c9538af772961f005a685aa33
Christina Whyte <kurisu.whyte@gmail.com>
2013-08-31 01:17:08 -0300

    Fixes Version field.

--- multi.el
+++ multi.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (c) 2013 Christina Whyte <kurisu.whyte@gmail.com>
 
-;; Version 1.0.0
+;; Version: 1.0.0
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: multimethod generic predicate dispatch
 ;; Author: Christina Whyte <kurisu.whyte@gmail.com>
